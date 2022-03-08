d23b5082fb80f4572d7b06dff1a2f179e503c5fc
pratiktamhane <96224999+pratiktamhane@users.noreply.github.com>
2022-03-09 00:41:38 +0530

    Add files via upload

---
+++ readFormData.js
@@ -0,0 +1,8 @@
+function readFormData() {
+    var formData = {};
+    formData["fullName"] = document.getElementById("fullName").value;
+    formData["empCode"] = document.getElementById("empCode").value;
+    formData["salary"] = document.getElementById("salary").value;
+    formData["city"] = document.getElementById("city").value;
+    return formData;
+}
